dab886d9c3c72ccd751a34920d7d0ad2447479e7
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-12-13 19:03:46 -0500

    add a get /api/time to show responding to api request with server

--- app/b/page.tsx
+++ app/b/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function B() {
-  return <div>b</div>;
+  const [time, setTime] = useState<string>("");
+
+  useEffect(() => {
+    fetch("/api/time")
+      .then((res) => res.json())
+      .then((data) => setTime(data.time));
+  }, []);
+
+  return <div>Current time: {time}</div>;
 }
--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { createServer } from "http";
-import { parse } from "url";
 import next from "next";
+import { parse } from "url";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -10,12 +10,20 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url!, true);
+
+    // GET /api/time
+    if (parsedUrl.pathname === "/api/time") {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ time: new Date().toISOString() }));
+      return;
+    }
+
     handle(req, res, parsedUrl);
   }).listen(port);
 
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? "development" : process.env.NODE_ENV
-    }`,
+    }`
   );
 });
